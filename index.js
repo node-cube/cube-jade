7f8f36a1d73deb05144bef4d4dcae296705667c8
剪巽 <jianxun.zxl@taobao.com>
2015-08-19 14:07:40 +0800

    process html code when query is an \.html query

--- index.js
+++ index.js
@@ -18,10 +18,15 @@ JadeProcessor.prototype = {
     var res = {};
     var root = options.root;
     var fpath = path.join(root, file);
+    var resFun;
+    var flagHtml = false;
+    if (/\.html$/.test(options.qpath)) {
+      flagHtml = true;
+    }
     code = fs.readFileSync(fpath, 'utf8').toString();
     res.source = code;
     try {
-      var resFun = jade.compileClient(code, {filename: fpath});
+      resFun = jade.compile(code, {filename: fpath});
       code = resFun.toString();
     } catch (e) {
       e.message += '\n file:' + file;
@@ -35,8 +40,12 @@ JadeProcessor.prototype = {
          ';module.exports = template;';
       res.wraped = this.cube.wrapTemplate(options.qpath, wraped, ['jade_runtime']);
     }
-    res.code = code;
 
+    if (flagHtml) {
+      res.code = resFun({});
+    } else {
+      res.code = code;
+    }
     callback(null, res);
   }
 };
